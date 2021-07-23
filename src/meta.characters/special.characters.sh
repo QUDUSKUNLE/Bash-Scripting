47cba2cb8c7f9f21ba6dc1978833e9451c0fa67e
QUDUSKUNLE <quduskunle@gmail.com>
2021-07-23 15:10:16 +0100

    add meta.characters

---
+++ src/meta.characters/special.characters.sh
@@ -0,0 +1,13 @@
+# !/bin/bash
+
+echo Hello\; World
+echo "I have \$1200."
+
+echo '<-$1500.**>; (update?) [y|n]'
+
+echo "It's Shell Programming"
+
+DATE=`date`
+
+echo "Current Date: $DATE"
+echo "Current Date: ${DATE}"
